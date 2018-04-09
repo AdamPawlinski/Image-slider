2e386b6239663e71aa807a3fee43b366bdba30e2
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-09 06:19:50 +0200

    control arrows works properly, but without styling, slides numbers added, styling in progress

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(function(){
-  carouselStart();
   var carouselList = $("#images");
   function moveSlide(){
     var firstItem = carouselList.find("li:first");
@@ -7,38 +6,26 @@ $(function(){
     lastItem.after(firstItem);
     carouselList.css({marginLeft:0});
   }
+
+  function moveSlidePrev(){
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    firstItem.before(lastItem);
+    carouselList.css({marginLeft:0});
+  }
+
   function changeSlide(){
     carouselList.animate({marginLeft:-500}, 3000, moveSlide);
   }
+
+  $('#left').on('click', function(){
+    carouselList.animate({marginLeft:500}, moveSlidePrev);
+  });
+  $('#right').on('click', function(){
+    carouselList.animate({marginLeft:-500}, moveSlide)
+  });
+
   function carouselStart(){
     setInterval(changeSlide, 3000);
-  }
-  // $('.image1').on('click', function(){
-  //   $('#carousel').carousel(0);
-  // })
-  // $('.image2').on('click', function(){
-  //   $('#carousel').carousel(1);
-  // })
-  // $('.image3').on('click', function(){
-  //   $('#carousel').carousel(2);
-  // })
-  // $('.image4').on('click', function(){
-  //   $('#carousel').carousel(3);
-  // })
-  // $('.image5').on('click', function(){
-  //   $('#carousel').carousel(4);
-  // })
-  // $('#carousel').carousel({pause: hover});
-  // $('#left').on('click', function(){
-  //   $('#carousel').carousel('next');
-  // })
-  // $('#right').on('click', function(){
-  //   $('#carousel').carousel('prev');
-  // })
-  $(.left).on('click', function(){
-    carouselList.animate({left:500})
-  }
-  $(.right).on('click', function(){
-    carouselList.animate({left:-500})
   }
 });
