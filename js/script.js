eaeddf490431b4a23806b7ae0e49e4249584ee4b
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-10 23:09:17 +0200

    finished, I think so...

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 $(function(){
-  var start = function carouselStart(){
-    setInterval(changeSlide, 3000);
-  }
-
   var carouselList = $("#images");
+  var interval = setInterval(changeSlide, 3000);
+
   function moveSlide(){
     var firstItem = carouselList.find("li:first");
     var lastItem = carouselList.find("li:last");
     lastItem.after(firstItem);
     carouselList.css({marginLeft:0});
+  }
+
+  function carouselStart(){
+    interval;
+  }
+
+  function changeSlide(){
+    carouselList.animate({marginLeft:-500}, 3000, moveSlide);
   }
 
   function moveSlidePrev(){
@@ -18,14 +24,20 @@ $(function(){
     carouselList.css({marginLeft:0});
   }
 
-  function changeSlide(){
-    carouselList.animate({marginLeft:-500}, 3000, moveSlide);
-  }
-
   $('#left').on('click', function(){
+    clearInterval(interval);
     carouselList.animate({marginLeft:500}, 1000, moveSlidePrev);
+    setInterval(changeSlide, 3000);
   });
   $('#right').on('click', function(){
-    carouselList.animate({marginLeft:-500}, 1000, moveSlide)
-  });  
+    clearInterval(interval);
+    carouselList.animate({marginLeft:-500}, 1000, moveSlide);
+    setInterval(changeSlide, 3000);
+  });
+
+  $('.slides-number > a').on('click', function(){
+    clearInterval(interval);
+  })
+
+  carouselStart();
 });
